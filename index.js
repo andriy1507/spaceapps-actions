677d760fee3d7e65382856e9244d1310779a3eff
AndriyGoryachok <andriy1507@hotmail.com>
2021-12-07 23:43:04 +0200

    Added octokit initialization

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 core.info('Hello world!!!');
+
+const token = core.getInput('GITHUB_TOKEN')
+const octokit = github.getOctokit(token)
+octokit.log.info('Octokit initialized')
